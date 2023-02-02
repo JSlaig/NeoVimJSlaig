dc574a4056f531f6c29758ec9a35f6cd7d085eaf
Jesús Salinero Guzmán <jsaligz@gmail.com>
2023-02-02 22:58:51 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,4 +2,12 @@ require("core.keymaps")
 require("core.plugins")
 require("core.plugin_config")
 
-vim.wo.number = true
+local o = vim.o
+local wo = vim.wo
+
+wo.number = true
+o.clipboard = "unnamedplus"
+
+o.splitright = true
+o.hidden = true
+o.lazyredraw = true
