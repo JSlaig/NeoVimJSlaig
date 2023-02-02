6880fa16b223650d885a49becb570fee50efa224
Jesús Salinero Guzmán <jsaligz@gmail.com>
2023-02-02 23:00:21 +0100

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,7 @@
+local mapper = function(mode, key, result)
+  vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
+end
+
 -- Set up the leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -15,4 +19,14 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+-- "<Space>w" to write
+mapper("n", "<Leader>w", ":w<CR>")
+
+-- "q" to quit
+mapper("n", "q", ":q<CR>")
+
+-- "<Space>qq" to fast quit
+mapper("n", "<Leader>qq", ":q!<CR>")
+
+-- "tt" To duplicate line
+mapper("n", "tt", ":t.<CR>")
