fe9c08d9ad3fb054a783e6f0845e63275a3d5eca
JSlaig <jsaligz@gmail.com>
2023-03-13 11:44:32 +0100

    Added the transparent background look

---
+++ after/plugin/colors.lua
@@ -0,0 +1,9 @@
+function remove_bg(color)
+  color = color or "gruvbox"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+  vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
+
+remove_bg()
