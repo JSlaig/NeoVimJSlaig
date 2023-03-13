5f7c0ba2f58964a2d68adc4d4772516a6f820fd3
JSlaig <jsaligz@gmail.com>
2023-03-13 11:33:06 +0100

    some modifications to telescope fuzzy finder, we just keep the find_files and git_files searches working

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,6 +1,4 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<c-o>', builtin.oldfiles, {})
-vim.keymap.set('n', '<c-g>', builtin.live_grep, {})
-vim.keymap.set('n', '<c-h>', builtin.help_tags, {})
+vim.keymap.set('n', '<c-p>', builtin.git_files, {})
+vim.keymap.set('n', '<c-o>', builtin.find_files, {})
