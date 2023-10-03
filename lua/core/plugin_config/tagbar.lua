ce5ba178d2502f2233fdde4c711facf28e012b72
jslaig <jsaligz@gmail.com>
2023-10-03 12:51:54 +0200

    Changed mapping from plugins to use '<leader>' key instead of 'Ctrl'

--- lua/core/plugin_config/tagbar.lua
+++ lua/core/plugin_config/tagbar.lua
@@ -1,4 +1,5 @@
 vim.g.tagbar_ctags_bin = 'C:\\ProgramData\\chocolatey\\bin\\ctags.exe'
 
 -- Toggle Tagbar with <ctrl + b> key
-vim.keymap.set('n', '<c-b>', ':TagbarToggle<CR>', {})
+vim.keymap.set('n', '<leader>b', ':TagbarToggle<CR>', {})
+
--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.live_grep, {})
-vim.keymap.set('n', '<c-o>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>p', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>o', builtin.find_files, {})
