0953a822e6d5f1cb57214999facfd70d40d46081
JSlaig <jsaligz@gmail.com>
2024-01-16 12:27:58 +0100

    changed binds for telescope

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>p', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>o', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
 -- Search and open on split
-vim.keymap.set('n', '<leader>so', ":vsplit<CR>|:Telescope find_files<CR>", {})
-vim.keymap.set('n', '<leader>vso', ":split<CR>|:Telescope find_files<CR>", {})
+vim.keymap.set('n', '<leader>sff', ":vsplit<CR>|:Telescope find_files<CR>", {})
+vim.keymap.set('n', '<leader>vsff', ":split<CR>|:Telescope find_files<CR>", {})
